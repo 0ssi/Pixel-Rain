bc3284cd9ccdc248ebd3ff5aa54af5dcf684d5b6
0ssi <oskar.fagernas@gmail.com>
2021-08-23 13:38:45 +0300

    e

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ image.addEventListener('load', function(){
     canvas.height = 352;
 
     let particlesArray = [];
-    const numberOfParticles = 15000;
+    const numberOfParticles = 7000;
     const detail = 1;
 
     ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
